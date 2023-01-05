fe8d5b8672f8b2124f301cc9300a70feb80fb663
Andrew Hoban <121863733+hobanEFC@users.noreply.github.com>
2023-01-05 10:29:19 +0000

    Update streamlit_app.py
    change the url

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,11 +27,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # convert the json into a normalised format
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-fruityvice_normalized 
 # format the data into a table
 streamlit.dataframe(fruityvice_normalized)
