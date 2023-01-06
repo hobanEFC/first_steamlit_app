a85abe67683da242bc8889b56a1c11894f7b82e5
Andrew Hoban <121863733+hobanEFC@users.noreply.github.com>
2023-01-06 14:49:04 +0000

    Update streamlit_app.py
    snowflake lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import snowflake.connector
+
 import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -35,3 +35,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # format the data into a table
 streamlit.dataframe(fruityvice_normalized)
+
+
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
